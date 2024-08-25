004420ceaab8d503f877e4b0f7efe85487bafb60
raja9112 <rrn.rajarajan@gmail.com>
2024-08-25 14:33:25 -0400

    Configured settings.py and urls.py files for media configuration in project level

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -126,6 +126,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+# Media files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
