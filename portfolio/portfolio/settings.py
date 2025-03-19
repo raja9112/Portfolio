52be5549cb9fb6d861d222fa1969f197700cdefb
raja9112 <rrn.rajarajan@gmail.com>
2025-03-20 04:29:47 +0530

    Updated experience year and updated settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
-
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
